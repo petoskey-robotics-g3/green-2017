eef85dae3c46fb6b1d3f4f75a52541469a196a4b
g3-green <g3robotics@gmail.com>
2017-11-07 19:01:04 -0500

    fixing merge conflicts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.TouchSensor;
 
 /**
  * Created by Administrator on 9/25/2016.
@@ -15,6 +14,7 @@ public class TankDrive extends OpMode {
     DcMotor leftMotor,rightMotor, liftMotor,grabberMotor;
     boolean leftBumper,rightBumper =  false;
 //    TouchSensor touch;
+
 
     @Override
     public void init() {
@@ -36,30 +36,31 @@ public class TankDrive extends OpMode {
 
         float rightTrigger = gamepad2.right_trigger;
         float leftTrigger = gamepad2.left_trigger;
-        boolean rightBumper = gamepad2.right_bumper;
-        boolean leftBumper = gamepad2.left_bumper;
+        float rightStick = gamepad2.right_stick_x;
+        float leftStick = gamepad2.left_stick_x;
 
 //        leftY = (leftY == 0) ? -gamepad2.left_stick_y : leftY;
 //        rightY = (rightY == 0) ? -gamepad2.right_stick_y : rightY;
 
         telemetry.addData("left joystick value", leftY);
         telemetry.addData("right joystick value", rightY);
-        leftBumper = gamepad2.left_bumper;
-        rightBumper = gamepad2.right_bumper;
+        //leftBumper = gamepad2.left_bumper;
+        //rightBumper = gamepad2.right_bumper;
 
-        leftMotor.setPower(leftY);
+        leftMotor.setPower(-leftY);
         rightMotor.setPower(rightY);
 
         grabberMotor.setPower(clawStick);
 
-        if(leftBumper){
-            telemetry.addData("left bumper", leftBumper);
-            grabberMotor.setPower(.2);
-        }
-        if(rightBumper) {
-            telemetry.addData("right bumper", rightBumper);
-            grabberMotor.setPower(.2);
-        }
+//        if(leftBumper){
+//            telemetry.addData("left bumper", leftBumper);
+//            grabberMotor.setPower(.2);
+//        }
+//        if(rightBumper) {
+//            telemetry.addData("right bumper", rightBumper);
+//            grabberMotor.setPower(.2);
+//        }
+
         if(rightTrigger > 0.0) {
             liftMotor.setPower(-rightTrigger);
 
@@ -69,8 +70,13 @@ public class TankDrive extends OpMode {
             liftMotor.setPower(0.0);
         }
 
+        if(rightStick > 0.0) {
+            grabberMotor.setPower(-rightStick);
 
+        }else if(leftStick > 0.0) {
+            grabberMotor.setPower(leftStick);
+        } else {
+            grabberMotor.setPower(0.0);
+        }
     }
-
-
 }
